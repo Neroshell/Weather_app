88edb3718dda199f98bee52f12d80051688cea89
Udezi Oghenero <udezioghenero@mail.com>
2023-09-21 01:46:11 +0100

    first push

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# Weather_app
+Weather App
+
+This is a simple weather app that allows users to check the current weather conditions for a specific city. It provides real-time weather information, including temperature, humidity, wind speed, and more.
+
+Features
+Search for a City: Users can enter the name of a city to get the current weather information for that location.
+
+Real-Time Weather Data: The app fetches real-time weather data from the OpenWeatherMap API, providing accurate and up-to-date information.
+
+Background Video: The app features a dynamic background video that adds a visual element to the user interface.
+
+Error Handling: If there are any errors in the user's input or while fetching data, the app displays informative error messages.
+
+Usage
+Enter the name of a city in the input box.
+
+Click the search button (or press Enter) to fetch the weather data for the specified city.
+
+The app will display the current weather conditions, including temperature, humidity, and more.
+
+The app will also show an icon representing the weather condition, such as rain, snow, clouds, or clear skies.
+
+If there are any errors, such as an empty input or an issue with the API request, the app will display error messages to inform the user.
+
+The app also provides a default weather display for a predefined city (Lagos) upon initial load.
+
+Screenshots
+[Insert screenshots here if desired]
+
+Installation
+To run this weather app locally, follow these steps:
+
+Clone the repository to your local machine.
+
+Open the index.html file in a web browser.
+
+You may need to replace the OpenWeatherMap API key in the JavaScript code with your own API key. Get an API key from OpenWeatherMap and replace the value of the key variable in app.js with your key.
+
+Ensure that the video file (moving_clouds.mp4) and other assets are available in the correct file paths specified in the HTML and CSS.
+
+Start using the app by entering a city name and clicking the search button.
+
+Technologies Used
+HTML
+CSS
+JavaScript
+Credits
+Weather data provided by OpenWeatherMap
+
+Background video source: Pixabay.com
+
+Fonts: Roboto
+
+License
+This project is licensed under the MIT License - see the LICENSE file for details.
---
+++ app.js
@@ -0,0 +1,129 @@
+const inputBox = document.querySelector('.input-box');
+const searchBox = document.querySelector('.search-box');
+const errorMessage = document.querySelector('.error');
+const key = '8be7cd2f8909a78ab2562af13a549d3e';
+const degree = document.querySelector('.degree');
+const main = document.querySelector('.main');
+const cityName = document.querySelector('.cityname');
+const realFeel = document.querySelector('.real_feel');
+const humidity = document.querySelector('.humid');
+const wind = document.querySelector('.wind');
+const pressure = document.querySelector('.pressure');
+const weatherImage = document.querySelector('.weather_img');
+const vidSource = document.querySelector('.video-source');
+const empty = document.querySelector('.empty');
+const videoBackground = document.querySelector('.video-background');
+
+function updateRealTimeDate() {
+    const dateElement = document.querySelector('.date_time');
+    const currentDate = new Date();
+    const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true };
+    const formattedDate = currentDate.toLocaleDateString('en-US', options);
+    dateElement.textContent = formattedDate;
+}
+
+function getCountry(country){
+    const countryName = new Intl.DisplayNames(['en'], {type: "region"});
+    return countryName.of(country);
+}
+
+searchBox.addEventListener('click', fetchData);
+
+async function fetchData(){
+    const city = inputBox.value;
+
+    if (city === '') {
+        empty.innerHTML = 'Kindly search for a city';
+        errorMessage.innerHTML = ''; // Clear any previous error message
+        return; 
+    }
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error("Couldn't fetch weather data, please try again");
+        } else {
+            errorMessage.innerHTML = ''; // Clear any previous error message
+            empty.innerHTML = ''; // Clear the empty input message
+        }
+
+        const data = await response.json();
+        let region = getCountry(data.sys.country);
+        main.innerHTML = data.weather[0].main;
+        degree.innerHTML = Math.round(data.main.temp) + '&deg;C';
+        cityName.innerHTML = data.name + ', ' + region;
+        realFeel.innerHTML =  Math.round(data.main.feels_like) + '&deg;C';
+        humidity.innerHTML = data.main.humidity + '%';
+        pressure.innerHTML = data.main.pressure + ' hPa';
+        wind.innerHTML = data.wind.speed + ' km/h';
+
+        if (data.weather[0].main === 'Rain') {
+            weatherImage.src = 'images/rainy.png';
+        } else if (data.weather[0].main === 'Snow') {
+            weatherImage.src = 'images/snow.png';
+        } else if (data.weather[0].main === 'Clouds') {
+            weatherImage.src = 'images/cloudy.png';   
+        } else if (data.weather[0].main === 'Clear') {
+            weatherImage.src = 'images/clear.png';
+        };
+    }
+    catch (err) {
+        console.log(err.message)
+        errorMessage.innerHTML = err.message;
+    }
+
+    updateRealTimeDate()
+}
+
+
+async function defaultData(){
+    
+
+   
+
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=lagos&appid=${key}&units=metric`;
+
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error("Couldn't fetch weather data, please try again");
+        } 
+
+        const data = await response.json();
+        let region = getCountry(data.sys.country);
+        main.innerHTML = data.weather[0].main;
+        degree.innerHTML = Math.round(data.main.temp) + '&deg;C';
+        cityName.innerHTML = data.name + ', ' + region;
+        realFeel.innerHTML =  Math.round(data.main.feels_like) + '&deg;C';
+        humidity.innerHTML = data.main.humidity + '%';
+        pressure.innerHTML = data.main.pressure + ' hPa';
+        wind.innerHTML = data.wind.speed + ' km/h';
+
+        if (data.weather[0].main === 'Rain') {
+            weatherImage.src = 'images/rainy.png';
+        } else if (data.weather[0].main === 'Snow') {
+            weatherImage.src = 'images/snow.png';
+        } else if (data.weather[0].main === 'Clouds') {
+            weatherImage.src = 'images/cloudy.png'; 
+              
+        } else if (data.weather[0].main === 'Clear') {
+            weatherImage.src = 'images/clear.png';
+
+        };
+    }
+    catch (err) {
+        console.log(err.message)
+        errorMessage.innerHTML = err.message;
+    }
+
+    updateRealTimeDate()
+}
+
+window.addEventListener('load', defaultData);
+
+
